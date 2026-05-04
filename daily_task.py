7dbe7644737a80e018d41b7450efae6665d22045
Fitrifazlee <fitrifazleee@gmail.com>
2026-05-04 15:24:02 +0800

    Emergency cleanup: Only tracking essential Flask files

--- daily_task.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-
-#buat long entry setiap kali runs os
-
-with open("/home/galactic/script_log.txt", "a") as f:
-	f.write(f'Script ran successfully at: {datetime.now()}\n")
-
-print("Task Completed!")
